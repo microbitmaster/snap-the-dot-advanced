5eb1c7f1dff35cd2a9fe9689141554e78a5911a9
microbitmaster <ddaasseerr882@gmail.com>
2025-01-01 22:20:04 -0800

    another dot (top=logo (v2 micro bit only) middle=a bottom=b)

--- main.ts
+++ main.ts
@@ -12,16 +12,32 @@ input.onButtonPressed(Button.B, function () {
         game.gameOver()
     }
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (sprite_3.get(LedSpriteProperty.X) == 2) {
+        game.addScore(1)
+    } else {
+        game.gameOver()
+    }
+})
+let sprite_3: game.LedSprite = null
 let sprite: game.LedSprite = null
 let sprite_2: game.LedSprite = null
 sprite_2 = game.createSprite(2, 2)
 sprite = game.createSprite(2, 3)
+sprite_3 = game.createSprite(2, 1)
 basic.forever(function () {
-    sprite_2.ifOnEdgeBounce()
+    basic.pause(randint(0, 1000))
+    sprite_3.move(1)
 })
 basic.forever(function () {
     basic.pause(randint(0, 1000))
     sprite_2.move(1)
+})
+basic.forever(function () {
+    sprite_2.ifOnEdgeBounce()
+})
+basic.forever(function () {
+    sprite_3.ifOnEdgeBounce()
 })
 basic.forever(function () {
     sprite.ifOnEdgeBounce()
