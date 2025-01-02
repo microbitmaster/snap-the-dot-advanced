49209a56697431a824f3ae203f5ab777c9e10c1c
microbitmaster <ddaasseerr882@gmail.com>
2025-01-01 22:16:06 -0800

    same as the original snap the dot advanced but with another unpredictable dot

--- main.ts
+++ main.ts
@@ -5,12 +5,28 @@ input.onButtonPressed(Button.A, function () {
         game.gameOver()
     }
 })
+input.onButtonPressed(Button.B, function () {
+    if (sprite.get(LedSpriteProperty.X) == 2) {
+        game.addScore(1)
+    } else {
+        game.gameOver()
+    }
+})
+let sprite: game.LedSprite = null
 let sprite_2: game.LedSprite = null
 sprite_2 = game.createSprite(2, 2)
+sprite = game.createSprite(2, 3)
 basic.forever(function () {
     sprite_2.ifOnEdgeBounce()
 })
 basic.forever(function () {
     basic.pause(randint(0, 1000))
     sprite_2.move(1)
+})
+basic.forever(function () {
+    sprite.ifOnEdgeBounce()
+})
+basic.forever(function () {
+    basic.pause(randint(0, 1000))
+    sprite.move(1)
 })
